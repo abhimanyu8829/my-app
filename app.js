b5c2466f1c125ca2e707b0e27e69ece3847f14ec
Abhimanyu Bhardwaj <108457163+abhimanyu8829@users.noreply.github.com>
2026-02-26 15:11:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -83,3 +83,14 @@ app.get("/", (req, res) => {
   </html>
   `);
 });
+
+
+
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
